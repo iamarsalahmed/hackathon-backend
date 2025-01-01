d01ade8b2f34eddc9c860a9624134b9475852f8a
Arsal Ahmed <arsalahmed1337@gmail.com>
2025-01-01 20:54:31 +0500

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
     origin: [
       "https://foodapp-six-lemon.vercel.app",  // without the trailing slash
       "https://foodapp-six-lemon.vercel.app/",  // with the trailing slash
-        "http://localhost:3001"
+        "http://localhost:3000"
     ],
     credentials: true,
   }));
