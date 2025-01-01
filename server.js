ae9d8b9a5b448a7f035594ef96669608d8f350a8
Arsal Ahmed <arsalahmed1337@gmail.com>
2025-01-01 20:05:23 +0500

    working

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 const PORT = 3001
 dotenv.config();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://foodapp-six-lemon.vercel.app/",
     credentials: true,
   }));
 app.use(express.json());
